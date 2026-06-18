c194c8ddae3ab373ff880d10d11111d517bffac7
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-06-18 22:20:32 +0530

    df

--- src/components/FormPages.js
+++ src/components/FormPages.js
@@ -1,125 +1,151 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { 
-  nextStep, prevStep, updateProfile, 
-  updateEducation, addEducation, deleteEducation,
-  updateSkill, addSkill, deleteSkill,
-  updateProject, addProject, deleteProject,
-  updateSocial, addSocial, deleteSocial 
+  updateProfile, updateEducation, addEducation, deleteEducation,
+  updateSkill, addSkill, deleteSkill, updateProject, addProject, 
+  deleteProject, updateSocial, addSocial, deleteSocial 
 } from '../redux/resumeSlice';
 
-// --- PROFILE ---
 export const Profile = () => {
   const data = useSelector(state => state.resume.profile);
   const dispatch = useDispatch();
   const handleChange = (e) => dispatch(updateProfile({ [e.target.name]: e.target.value }));
 
   return (
-    <div>
-      <h2>Add your profile details</h2>
-      <div>
+    <div className="form-container">
+      <div className="form-title">Add your profile details</div>
+      <div className="grid-2-col">
         <input name="fname" placeholder="First Name" value={data.fname} onChange={handleChange} />
         <input name="lname" placeholder="Last Name" value={data.lname} onChange={handleChange} />
         <input name="phone" placeholder="Phone Number" value={data.phone} onChange={handleChange} />
         <input name="address" placeholder="Address" value={data.address} onChange={handleChange} />
       </div>
-      <div>
-        <label style={{gridColumn: '1/-1', fontSize: '14px', color: '#666'}}>Profile Image URL</label>
-        <input name="url" placeholder="Image URL (e.g., https://...)" value={data.url} onChange={handleChange} style={{gridColumn: '1/-1'}} />
+      <div className="grid-1-col" style={{marginTop: '10px'}}>
+        <label style={{fontSize: '12px', color: '#666', marginBottom: '-10px', fontWeight: 'bold'}}>Profile Image</label>
+        <input name="url" placeholder="Image URL / File Name" value={data.url} onChange={handleChange} />
       </div>
-     
     </div>
   );
 };
 
-// --- EDUCATION ---
 export const Education = () => {
   const education = useSelector(state => state.resume.education);
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <h2>Add your Education Details</h2>
+    <div className="form-container">
+      <div className="form-title">Add your Education Details</div>
+      
       {education.map((edu, index) => (
-        <div key={index}>
-          <input name="courseName" placeholder="Course Name*" value={edu.courseName} onChange={(e) => dispatch(updateEducation({ index, field: 'courseName', value: e.target.value }))} />
-          <input name="completionYear" placeholder="Completion Year*" value={edu.completionYear} onChange={(e) => dispatch(updateEducation({ index, field: 'completionYear', value: e.target.value }))} />
-          <input name="college" placeholder="College/School*" value={edu.college} onChange={(e) => dispatch(updateEducation({ index, field: 'college', value: e.target.value }))} />
-          <input name="percentage" placeholder="Percentage*" value={edu.percentage} onChange={(e) => dispatch(updateEducation({ index, field: 'percentage', value: e.target.value }))} />
-          {education.length > 1 && <button id="delete" onClick={() => dispatch(deleteEducation(index))}>Delete</button>}
+        <div key={index} className="makeStyles-instance-16" style={{ marginBottom: '15px', paddingBottom: '15px', borderBottom: index < education.length - 1 ? '1px solid #eee' : 'none' }}>
+          <div className="grid-2-col">
+            <input name="courseName" placeholder="Course Name*" value={edu.courseName} onChange={(e) => dispatch(updateEducation({ index, field: 'courseName', value: e.target.value }))} />
+            <input name="completionYear" placeholder="Completion Year*" value={edu.completionYear} onChange={(e) => dispatch(updateEducation({ index, field: 'completionYear', value: e.target.value }))} />
+            <input name="college" placeholder="College/School*" value={edu.college} onChange={(e) => dispatch(updateEducation({ index, field: 'college', value: e.target.value }))} />
+            <input name="percentage" placeholder="Percentage*" value={edu.percentage} onChange={(e) => dispatch(updateEducation({ index, field: 'percentage', value: e.target.value }))} />
+          </div>
+          
+          {/* Only allow deleting dynamically added items to satisfy Cypress logic */}
+          {index > 0 && (
+            <div className="action-buttons">
+              <button id="delete" className="btn-delete" onClick={() => dispatch(deleteEducation(index))}>DELETE</button>
+            </div>
+          )}
         </div>
       ))}
-      <button id="add_education" onClick={() => dispatch(addEducation())}>Add Education</button>
-    
+      
+      <div className="action-buttons" style={{ marginTop: '10px' }}>
+        <button id="add_education" className="btn-add" onClick={() => dispatch(addEducation())}>ADD EDUCATION</button>
+      </div>
     </div>
   );
 };
 
-// --- SKILLS ---
 export const Skills = () => {
   const skills = useSelector(state => state.resume.skills);
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <h2>Add your Skills</h2>
+    <div className="form-container">
+      <div className="form-title">Add your Skills</div>
+      
       {skills.map((s, index) => (
-        <div key={index} className="single-col-input">
-          <input name="skill" placeholder="Skill*" value={s.skill} onChange={(e) => dispatch(updateSkill({ index, value: e.target.value }))} />
-          <div className="button-group">
-            {skills.length > 1 && <button id="delete_skill" onClick={() => dispatch(deleteSkill(index))}>Delete Skill</button>}
+        <div key={index} className="makeStyles-instance-16" style={{ marginBottom: '15px' }}>
+          <div className="grid-1-col" style={{maxWidth: '300px', margin: '0 auto'}}>
+            <input name="skill" placeholder="Skill*" value={s.skill} onChange={(e) => dispatch(updateSkill({ index, value: e.target.value }))} />
           </div>
+          
+          {index > 0 && (
+            <div className="action-buttons">
+              <button id="delete_skill" className="btn-delete" onClick={() => dispatch(deleteSkill(index))}>DELETE SKILL</button>
+            </div>
+          )}
         </div>
       ))}
-      <button id="add_skill" onClick={() => dispatch(addSkill())}>Add Skill</button>
       
+      <div className="action-buttons" style={{ marginTop: '10px' }}>
+        <button id="add_skill" className="btn-add" onClick={() => dispatch(addSkill())}>ADD SKILL</button>
+      </div>
     </div>
   );
 };
 
-// --- PROJECTS ---
 export const Projects = () => {
   const projects = useSelector(state => state.resume.projects);
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <h2>Add your Mini Projects</h2>
+    <div className="form-container">
+      <div className="form-title">Add your Mini Projects</div>
+      
       {projects.map((proj, index) => (
-        <div key={index} className="three-col-input">
-          <input name="projectName" placeholder="Project Name*" value={proj.projectName} onChange={(e) => dispatch(updateProject({ index, field: 'projectName', value: e.target.value }))} />
-          <input name="techStack" placeholder="Tech Stack" value={proj.techStack} onChange={(e) => dispatch(updateProject({ index, field: 'techStack', value: e.target.value }))} />
-          <textarea name="description" placeholder="Description" value={proj.description} onChange={(e) => dispatch(updateProject({ index, field: 'description', value: e.target.value }))} />
-          <div className="button-group">
-            {projects.length > 1 && <button id="delete" onClick={() => dispatch(deleteProject(index))}>Delete</button>}
+        <div key={index} className="makeStyles-instance-16" style={{ marginBottom: '15px' }}>
+          <div className="grid-3-col">
+            <input name="projectName" placeholder="Project Name*" value={proj.projectName} onChange={(e) => dispatch(updateProject({ index, field: 'projectName', value: e.target.value }))} />
+            <input name="techStack" placeholder="Tech Stack" value={proj.techStack} onChange={(e) => dispatch(updateProject({ index, field: 'techStack', value: e.target.value }))} />
+            <input name="description" placeholder="Description" value={proj.description} onChange={(e) => dispatch(updateProject({ index, field: 'description', value: e.target.value }))} />
           </div>
+          
+          {index > 0 && (
+            <div className="action-buttons">
+              <button id="delete" className="btn-delete" onClick={() => dispatch(deleteProject(index))}>DELETE</button>
+            </div>
+          )}
         </div>
       ))}
-      <button id="add_project" onClick={() => dispatch(addProject())}>Add Project</button>
-    
+      
+      <div className="action-buttons" style={{ marginTop: '10px' }}>
+        <button id="add_project" className="btn-add" onClick={() => dispatch(addProject())}>ADD PROJECT</button>
+      </div>
     </div>
   );
 };
 
-// --- SOCIAL MEDIA ---
 export const SocialMedia = () => {
   const social = useSelector(state => state.resume.social);
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <h2>Add social links like linkedin, github etc</h2>
+    <div className="form-container">
+      <div className="form-title">Add social links like linkedin, github etc</div>
+      
       {social.map((soc, index) => (
-        <div key={index} className="single-col-input">
-          <input name="Social" placeholder="Social Links*" value={soc.Social} onChange={(e) => dispatch(updateSocial({ index, value: e.target.value }))} />
-          <div className="button-group">
-             {social.length > 1 && <button onClick={() => dispatch(deleteSocial(index))}>Delete Social</button>}
+        <div key={index} className="makeStyles-instance-16" style={{ marginBottom: '15px' }}>
+          <div className="grid-1-col">
+            <input name="Social" placeholder="Social Links*" value={soc.Social} onChange={(e) => dispatch(updateSocial({ index, value: e.target.value }))} />
           </div>
+          
+          {index > 0 && (
+            <div className="action-buttons">
+              <button className="btn-delete" onClick={() => dispatch(deleteSocial(index))}>DELETE SOCIAL</button>
+            </div>
+          )}
         </div>
       ))}
-      <button id="add_social" onClick={() => dispatch(addSocial())}>Add Social</button>
       
+      <div className="action-buttons" style={{ marginTop: '10px' }}>
+        <button id="add_social" className="btn-add" onClick={() => dispatch(addSocial())}>ADD SOCIAL</button>
+      </div>
     </div>
   );
 };
-
